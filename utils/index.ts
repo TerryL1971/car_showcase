1570eb3b49a829da7e3450e25a5e7b5869200e46
TerryL1971 <83511726+TerryL1971@users.noreply.github.com>
2024-02-26 16:28:00 +0100

    Cleaned duplicates
    removed all ""2.tsx files.

--- utils/index.ts
+++ utils/index.ts
@@ -16,17 +16,14 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-  // Get the current URL search params
   const searchParams = new URLSearchParams(window.location.search);
 
-  // Set the specified search parameter to the given value
-  searchParams.set(type, value);
+    searchParams.set(type, value)
 
-  // Set the specified search parameter to the given value
-  const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`
 
-  return newPathname;
-};
+    return newPathname;
+}
 
 export const deleteSearchParams = (type: string) => {
   // Set the specified search parameter to the given value
@@ -72,3 +69,5 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
 
   return `${url}`;
 } 
+
+  
